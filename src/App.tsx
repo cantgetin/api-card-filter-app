adc0a97bd6bc946a0482970b77ee4dc8577ef3b8
cantgetin <84032933+cantgetin@users.noreply.github.com>
2022-11-28 14:00:31 +0500

    Installed spinners-react, displaying spinner on loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import List from "./components/List";
 import ProductCard from "./components/ProductCard";
 import {Product} from "./types/types";
 import axios from "axios";
+import {SpinnerCircularFixed} from 'spinners-react';
 
 function App() {
 
-    const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[] | null>(null);
 
     useEffect(() => {
-        axios.get<{products: Product[]}>('https://dummyjson.com/products')
+        axios.get<{ products: Product[] }>('https://dummyjson.com/products')
             .then(r => setProducts(r.data.products));
     }, [])
 
     return (
-        <div className="App bg-slate-100 w-screen">
+        <div className="App bg-slate-100 w-screen max-w-full">
             <Header/>
-            {
-                products ?
-                <List items={products} renderItem={(product: Product) => <ProductCard product={product} key={product.id}/>}/>
-                : null
+            {products ?
+                <List items={products}
+                      renderItem={(product: Product) => <ProductCard product={product} key={product.id}/>}/>
+                :
+                <div className="flex justify-center items-center h-screen w-screen">
+                    <SpinnerCircularFixed size={90} thickness={127} speed={112} color="var(--primaryBlue)" secondaryColor="#ececee"/>
+                </div>
             }
         </div>
     );
